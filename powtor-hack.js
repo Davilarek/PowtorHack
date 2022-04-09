5f0af6c0cb77593184f74cac0d89fb0b91f0f92c
Daniel <davilareko@gmail.com>
2022-04-09 15:55:24 +0200

    Update powtor-hack.js

--- powtor-hack.js
+++ powtor-hack.js
@@ -3,9 +3,9 @@ var open = window.XMLHttpRequest.prototype.open;
 var exeUrl = "";
 
 function openReplacement(method, url, async, user, password) {
-    this._url = url;
-    exeUrl = arguments[1];
-    return open.apply(this, arguments);
+	this._url = url;
+	exeUrl = arguments[1];
+	return open.apply(this, arguments);
 }
 window.XMLHttpRequest.prototype.open = openReplacement;
 
@@ -24,33 +24,39 @@ xhr.setRequestHeader("japa_phpsessid", header2[1]);
 var arr = ['instruction', 'question'];
 
 xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4) {
-        let resp = JSON.parse(xhr.responseText).pool
-        var myspans = document.getElementsByTagName("app-exercise-loader")[0].getElementsByClassName("ng-star-inserted");
-        for (var i = 0; i < myspans.length; i++) {
-            const contains = arr.some(element => {
-                if (myspans[i].className.indexOf(element) !== -1) {
-                    return true;
-                }
+	if (xhr.readyState === 4) {
+		let resp = JSON.parse(xhr.responseText).pool
+		var myspans = document.getElementsByTagName("app-exercise-loader")[0].getElementsByClassName("ng-star-inserted");
+		for (var i = 0; i < myspans.length; i++) {
+			const contains = arr.some(element => {
+				if (myspans[i].className.indexOf(element) !== -1) {
+					return true;
+				}
 
-                return false;
-            });
-            if (!contains) {
-                continue;
-            }
-            for (var i2 = 0; i2 < resp.length; i2++) {
-                var doc = new DOMParser().parseFromString(resp[i2].instruction, "text/html").documentElement;
+				return false;
+			});
+			if (!contains) {
+				continue;
+			}
+			for (var i2 = 0; i2 < resp.length; i2++) {
+				var instr = ""
+				if (resp[i2]["instruction"])
+					instr = resp[i2].instruction;
+				if (resp[i2]["question"])
+					instr = resp[i2].question;
+				
+				var doc = new DOMParser().parseFromString(instr, "text/html").documentElement;
 
-                var myspan = myspans[i].childNodes[0];
-                if (myspan.textContent == doc.textContent) {
-                    console.log(resp[i2].items);
-                    return;
-                    break;
-                }
-            }
-        }
-        console.log("UWAGA! Nie odnaleziono odpowiedzi. Możesz spróbować odnaleźć je ręcznie. ");
-        console.log(resp);
-    }
+				var myspan = myspans[i].childNodes[0];
+				if (myspan.textContent == doc.textContent) {
+					console.log(resp[i2].items);
+					return;
+					break;
+				}
+			}
+		}
+		console.log("UWAGA! Nie odnaleziono odpowiedzi. Możesz spróbować odnaleźć je ręcznie. ");
+		console.log(resp);
+	}
 };
 xhr.send();
